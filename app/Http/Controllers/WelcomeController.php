b5498d0fc8b983bb160c76fb726bf939ec3f9e9a
pringgojs <odyinggo@gmail.com>
2018-09-04 10:52:44 +0700

    test connection

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,5 +1,7 @@
 <?php namespace App\Http\Controllers;
 
+use \App\Models\Mahasiswa;
+
 class WelcomeController extends Controller {
 
 	/*
@@ -30,7 +32,8 @@ class WelcomeController extends Controller {
 	 */
 	public function index()
 	{
-		return view('welcome');
+		$mahasiswa = Mahasiswa::find(2033);
+		dd($mahasiswa);
 	}
 
 }
